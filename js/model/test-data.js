51e61ce47195c20438c9eccb98bf4e07e4d15dc8
SlavaMilin <slava.milin@gmail.com>
2018-06-10 16:15:11 +0300

    add more test data

--- js/model/test-data.js
+++ js/model/test-data.js
@@ -90,6 +90,72 @@ const testData = [
     ]
   },
   {
+    type: `two-of-two`,
+    question: `Угадайте для каждого изображения фото или рисунок?`,
+    answers: [
+      {
+        image: {
+          url: `https://k32.kn3.net/5C7060EC5.jpg`,
+          width: 468,
+          height: 458
+        },
+        type: `painting`
+      },
+      {
+        image: {
+          url: `https://i.redd.it/l08jq66vul2y.jpg`,
+          width: 468,
+          height: 458
+        },
+        type: `photo`
+      }
+    ]
+  },
+  {
+    type: `two-of-two`,
+    question: `Угадайте для каждого изображения фото или рисунок?`,
+    answers: [
+      {
+        image: {
+          url: `https://k32.kn3.net/5C7060EC5.jpg`,
+          width: 468,
+          height: 458
+        },
+        type: `painting`
+      },
+      {
+        image: {
+          url: `https://i.redd.it/l08jq66vul2y.jpg`,
+          width: 468,
+          height: 458
+        },
+        type: `photo`
+      }
+    ]
+  },
+  {
+    type: `two-of-two`,
+    question: `Угадайте для каждого изображения фото или рисунок?`,
+    answers: [
+      {
+        image: {
+          url: `https://k32.kn3.net/5C7060EC5.jpg`,
+          width: 468,
+          height: 458
+        },
+        type: `painting`
+      },
+      {
+        image: {
+          url: `https://i.redd.it/l08jq66vul2y.jpg`,
+          width: 468,
+          height: 458
+        },
+        type: `photo`
+      }
+    ]
+  },
+  {
     type: `tinder-like`,
     question: `Угадай, фото или рисунок?`,
     answers: [

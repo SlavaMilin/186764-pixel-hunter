cbd1845b85eca702b839830289b094acbc4dddaa
SlavaMilin <slava.milin@gmail.com>
2018-06-20 15:54:24 +0300

    rename files

--- js/view/game-three.js
+++ js/view/game-three.js
@@ -1,6 +1,6 @@
 import AbstractView from "./abstract-view";
 
-export default class GameOneView extends AbstractView {
+export default class GameThree extends AbstractView {
   constructor(state) {
     super();
     this._state = state;
@@ -23,29 +23,16 @@ export default class GameOneView extends AbstractView {
   </div>
 </header>
 <div class="game">
-  <p class="game__task">Угадайте для каждого изображения фото или рисунок?</p>
-  <form class="game__content">
+  <p class="game__task">Найдите рисунок среди изображений</p>
+  <form class="game__content  game__content--triple">
     <div class="game__option">
-      <img src="http://placehold.it/468x458" alt="Option 1" width="468" height="458">
-      <label class="game__answer game__answer--photo">
-        <input name="question1" type="radio" value="photo">
-        <span>Фото</span>
-      </label>
-      <label class="game__answer game__answer--paint">
-        <input name="question1" type="radio" value="paint">
-        <span>Рисунок</span>
-      </label>
+      <img src="http://placehold.it/304x455" alt="Option 1" width="304" height="455">
+    </div>
+    <div class="game__option  game__option--selected">
+      <img src="http://placehold.it/304x455" alt="Option 1" width="304" height="455">
     </div>
     <div class="game__option">
-      <img src="http://placehold.it/468x458" alt="Option 2" width="468" height="458">
-      <label class="game__answer  game__answer--photo">
-        <input name="question2" type="radio" value="photo">
-        <span>Фото</span>
-      </label>
-      <label class="game__answer  game__answer--paint">
-        <input name="question2" type="radio" value="paint">
-        <span>Рисунок</span>
-      </label>
+      <img src="http://placehold.it/304x455" alt="Option 1" width="304" height="455">
     </div>
   </form>
   <div class="stats">
@@ -54,11 +41,11 @@ export default class GameOneView extends AbstractView {
       <li class="stats__result stats__result--slow"></li>
       <li class="stats__result stats__result--fast"></li>
       <li class="stats__result stats__result--correct"></li>
+      <li class="stats__result stats__result--wrong"></li>
       <li class="stats__result stats__result--unknown"></li>
+      <li class="stats__result stats__result--slow"></li>
       <li class="stats__result stats__result--unknown"></li>
-      <li class="stats__result stats__result--unknown"></li>
-      <li class="stats__result stats__result--unknown"></li>
-      <li class="stats__result stats__result--unknown"></li>
+      <li class="stats__result stats__result--fast"></li>
       <li class="stats__result stats__result--unknown"></li>
     </ul>
   </div>
--- js/view/game-two.js
+++ js/view/game-two.js
@@ -1,6 +1,6 @@
 import AbstractView from "./abstract-view";
 
-export default class GameThreeView extends AbstractView {
+export default class GameTwo extends AbstractView {
   constructor(state) {
     super();
     this._state = state;
@@ -23,16 +23,18 @@ export default class GameThreeView extends AbstractView {
   </div>
 </header>
 <div class="game">
-  <p class="game__task">Найдите рисунок среди изображений</p>
-  <form class="game__content  game__content--triple">
+  <p class="game__task">Угадай, фото или рисунок?</p>
+  <form class="game__content  game__content--wide">
     <div class="game__option">
-      <img src="http://placehold.it/304x455" alt="Option 1" width="304" height="455">
-    </div>
-    <div class="game__option  game__option--selected">
-      <img src="http://placehold.it/304x455" alt="Option 1" width="304" height="455">
-    </div>
-    <div class="game__option">
-      <img src="http://placehold.it/304x455" alt="Option 1" width="304" height="455">
+      <img src="http://placehold.it/705x455" alt="Option 1" width="705" height="455">
+      <label class="game__answer  game__answer--photo">
+        <input name="question1" type="radio" value="photo">
+        <span>Фото</span>
+      </label>
+      <label class="game__answer  game__answer--wide  game__answer--paint">
+        <input name="question1" type="radio" value="paint">
+        <span>Рисунок</span>
+      </label>
     </div>
   </form>
   <div class="stats">

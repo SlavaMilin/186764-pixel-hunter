81a1ffe8c167f61612ddcbeb421005b812f52675
SlavaMilin <slava.milin@gmail.com>
2018-06-22 22:43:54 +0300

    add rules render

--- js/application.js
+++ js/application.js
@@ -1,9 +1,11 @@
 import Model from "model/model";
 import Backend from "util/backend";
+import Intro from "./presenter/intro";
 
 
 export default class Application {
   static showRules() {
-
+    const intro = new Intro();
+    intro.render();
   }
 }

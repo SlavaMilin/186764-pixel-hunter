b95f3efd81eee33bd7d0372f77eaab203be69a5b
SlavaMilin <slava.milin@gmail.com>
2018-06-23 01:42:56 +0300

    rename and refactor info presenter

---
+++ js/presenter/intro-presenter.js
@@ -0,0 +1,10 @@
+import AbstractPresenter from "./abstract-presenter";
+import IntroView from "../view/intro-view";
+
+export default class IntroPresenter extends AbstractPresenter {
+  constructor(model) {
+    super(model);
+    this._view = new IntroView();
+    this._element = this._view.element;
+  }
+}

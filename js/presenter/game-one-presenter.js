94dee2ae8f333228942094004daab79cebe2a859
SlavaMilin <slava.milin@gmail.com>
2018-06-25 00:41:16 +0300

    import header to presentors

--- js/presenter/game-one-presenter.js
+++ js/presenter/game-one-presenter.js
@@ -1,10 +1,12 @@
 import AbstractPresenter from "./abstract-presenter";
 import GameOneView from "../view/game-one-view";
+import HeaderPresenter from "./header-presenter";
 
 export default class GameOnePresenter extends AbstractPresenter {
   constructor(model) {
     super(model);
     this._view = new GameOneView(model);
     this._element = this._view.element;
+    this._header = new HeaderPresenter(model);
   }
 }
--- js/presenter/game-three-presenter.js
+++ js/presenter/game-three-presenter.js
@@ -1,10 +1,12 @@
 import GameThreeView from "../view/game-three-view";
 import AbstractPresenter from "./abstract-presenter";
+import HeaderPresenter from "./header-presenter";
 
 export default class GameThreePresenter extends AbstractPresenter {
   constructor(model) {
     super(model);
     this._view = new GameThreeView(model);
     this._element = this._view.element;
+    this._header = new HeaderPresenter(model);
   }
 }

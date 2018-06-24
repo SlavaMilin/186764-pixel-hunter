48f23d04cc0787ae97eb8a5d53aaa585fc2f6e69
SlavaMilin <slava.milin@gmail.com>
2018-06-24 20:39:07 +0300

    add logic to change screen

--- js/app.js
+++ js/app.js
@@ -1,17 +1,61 @@
 import Model from "./model/model";
 import Backend from "./util/backend";
 import IntroPresenter from "./presenter/intro-presenter";
+import {GameType} from "./util/config";
+import GameThreePresenter from "./presenter/game-three-presenter";
+import GameTwoPresenter from "./presenter/game-two-presenter";
+import GameOnePresenter from "./presenter/game-one-presenter";
+import RulesPresenter from "./presenter/rules-presenter";
 
 
 export default class App {
-  static showRules() {
+  gameSelecter(type, model) {
+    if (type === GameType.ONE_OF_THREE) {
+      App.showGameThree(model);
+    }
+
+    if (type === GameType.TWO_OF_TWO) {
+      App.showGameTwo(model);
+    }
+
+    if (type === GameType.TINDER_LIKE) {
+      App.showGameOne(model);
+    }
+
+    if (type === GameType.RULES) {
+      App.showRules(model);
+    }
+  }
+
+  showIntro() {
     const model = new Model();
+    model.addSubscriber(this.gameSelecter);
     const backend = new Backend();
     const intro = new IntroPresenter(model);
     intro.render();
     backend.loadData().then((data) => {
       model.setData = data;
     });
+  }
+
+  static showRules(model) {
+    const presenter = new RulesPresenter(model);
+    presenter.render();
+  }
+
+  static showGameThree(model) {
+    const presenter = new GameThreePresenter(model);
+    presenter.render();
+  }
+
+  static showGameTwo(model) {
+    const presenter = new GameTwoPresenter(model);
+    presenter.render();
+  }
+
+  static showGameOne(model) {
+    const presenter = new GameOnePresenter(model);
+    presenter.render();
   }
 }
 

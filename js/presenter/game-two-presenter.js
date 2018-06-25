d385a7e68fe45dcdd722eed8a158a78aaf836e56
SlavaMilin <slava.milin@gmail.com>
2018-06-25 14:13:56 +0300

    remove extra

--- js/presenter/game-two-presenter.js
+++ js/presenter/game-two-presenter.js
@@ -1,12 +1,10 @@
 import AbstractPresenter from "./abstract-presenter";
 import GameTwoView from "../view/game-two-view";
-import HeaderPresenter from "./header-presenter";
 
 export default class GameTwoPresenter extends AbstractPresenter {
   constructor(model) {
     super(model);
     this._view = new GameTwoView(model);
     this._element = this._view.element;
-    this._header = new HeaderPresenter(model);
   }
 }

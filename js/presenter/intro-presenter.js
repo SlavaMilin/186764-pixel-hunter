572065fd7716156aa21eeace5c1323b5e264bc5a
SlavaMilin <slava.milin@gmail.com>
2018-07-01 01:02:56 +0300

    remove logic in intro view

--- js/presenter/intro-presenter.js
+++ js/presenter/intro-presenter.js
@@ -5,7 +5,6 @@ export default class IntroPresenter extends AbstractPresenter {
   constructor(model) {
     super(model);
     this._view = new IntroView(model);
-    this._view.onAnswer = this.onAnswer;
     this._element = this._view.element;
   }
 }
